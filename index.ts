7e4bf6a2fea9bfb7886a53182e75c9c86034b8c3
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-31 01:09:18 +0300

    fin

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ import { concatMapTo, take } from 'rxjs/operators';
 //emit val
 const interval$ = interval(2000);
 //emit value every second for 5 seconds
-const source = interval(1000).pipe(take(5));
+const srcInterval$ = interval(1000).pipe(take(5));
 /*
   ***Be Careful***: In situations like this where the source emits at a faster pace
   than the inner observable completes, memory issues can arise.
@@ -16,7 +16,7 @@ const source = interval(1000).pipe(take(5));
 // basicTimer will complete after 5 seconds, emitting 0,1,2,3,4
 const example = interval$.pipe(
   concatMapTo(
-    source,
+    srcInterval$,
     (firstInterval, secondInterval) => `${firstInterval} ${secondInterval}`
   )
 );
